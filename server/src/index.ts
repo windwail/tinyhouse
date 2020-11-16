08ab2e8bd7d2ec49975aeab56498cccc2298d18c
windwail <windwail>
2020-11-16 20:28:36 +0300

    Minimal graphQL server.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,17 @@
 import express from 'express';
 import {listings} from './listings';
 import bodyParser from 'body-parser';
+import {ApolloServer} from "apollo-server-express";
+import {schema} from "./graphql";
+
+
 
 const app = express();
 const port = 9000;
+const server = new ApolloServer({schema});
+server.applyMiddleware({app, path: '/api'});
 
-app.use(bodyParser.json());
 
-app.get('/listings', (_req,res) => {
-  res.send(listings);
-});
-
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for(let i = 0; i < listings.length; i++) {
-    if(listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send('Failed to delete listing');
-});
 
 app.listen(port);
 
